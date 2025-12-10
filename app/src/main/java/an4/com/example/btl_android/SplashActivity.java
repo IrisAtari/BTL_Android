84899cedb3e864f379dfc35f12f8a6601a4923b9
HoangHa412 <tunjin123@gmail.com>
2025-12-11 00:33:01 +0700

    Add Hello UI by HoangHa

---
+++ app/src/main/java/an4/com/example/btl_android/SplashActivity.java
@@ -0,0 +1,28 @@
+package an4.com.example.btl_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, CRUDActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
