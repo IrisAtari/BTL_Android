ecbfed397d15d5e25ef123e84c5443f9da92bef7
IrisAtari <nguyennhatminh1103@gmail.com>
2025-12-15 23:32:15 +0700

    +Final Commit after merge splash screen

--- app/src/main/java/an4/com/example/btl_android/SplashActivity.java
+++ app/src/main/java/an4/com/example/btl_android/SplashActivity.java
@@ -19,7 +19,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, CRUDActivity.class);
+                Intent intent = new Intent(SplashActivity.this, TabbedActivity.class);
                 startActivity(intent);
                 finish();
             }
